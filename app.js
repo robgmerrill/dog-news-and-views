d08f3f029f0a6a07c59a12f5e1c43b8d099f893b
Rob Merrill <robgmerrill@gmail.com>
2016-04-01 17:27:41 -0700

    modal completed

--- app.js
+++ app.js
@@ -18,4 +18,20 @@ $(document).ready(function() {
 
   });
 
+    //-----------------------------------------------
+  //   Modal
+  //-----------------------------------------------
+
+  $('.js-show-modal').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
+
 });
